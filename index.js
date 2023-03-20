0df6d4f13b4806b01766af962aeb3c62c4d6ab71
Miguelangel <miguelangel@ilogica.cl>
2023-03-20 10:20:58 -0300

    actualizo el repo para curso base de datos

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
-const {logErrors, errorHandler, boomErrorHandler} = require('./middleware/error.handler')
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
 
-app.get('/',(req, res) =>{
-    res.send("Hola mi server en express")
-})
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
 
-app.listen(PORT, () => {
-    console.log('ejecutando port ' +  PORT)
-})
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
 
 routerApi(app);
-//middleware de errores globales
-app.use(logErrors)
-app.use(boomErrorHandler)
-app.use(errorHandler)
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+
+app.listen(port, () => {
+  console.log('Mi port' +  port);
+});
